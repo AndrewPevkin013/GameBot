1a9bf807d4f0ad0f9c1ac71743ffd0151ec7219e
AP_0013 <368635@edu.itmo.ru>
2024-01-21 01:57:15 +0300

    added ttt game

--- GameBot.py
+++ GameBot.py
@@ -15,9 +15,6 @@ def handle_jumanji(message):
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if message.text.startswith("/jumanji"):
-        # command = message.text[9:]
-        # result = execute_csharp_application(command)
-        # bot.send_message(message.from_user.id, result)
         bot.send_message(message.from_user.id, "ДЖУМАНДЖИ")
     elif message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
@@ -25,19 +22,6 @@ def handle_text(message):
         bot.send_message(message.from_user.id, "И чтобы начать играть, вы слово /jumanji должны прокричать!")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
-
-# def execute_csharp_application(command):
-#   process = subprocess.Popen(['C:/Users/apevk/OneDrive/Рабочий стол/TicTacToe/TicTacToe/bin/Debug/net7.0/Tic Tac Toe.exe'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
-#   process.stdin.write(command + '\n')
-#   process.stdin.flush()
-#   result = process.stdout.read()
-#   process.stdin.close()
-#   process.wait()
-#   return result.strip()
-
-
-
 def main():
   bot.polling(none_stop=True, interval=0)
 
